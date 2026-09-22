45e02e2c4829ed57564706d351fc12af3dfff36b
theluchadora <160585859+theluchadora@users.noreply.github.com>
2026-09-23 00:50:16 +0300

    feat: Add role-based access control in middleware and define global session claims type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
     "/",
     "/sign-in(.*)",
     "/sign-up(.*)",
 ]);
+const isAdminRoute = createRouteMatcher(["/upload"]);
 
 export default clerkMiddleware(async (auth, req) => {
+
+  const { sessionClaims } = await auth();
+  const isAdmin = sessionClaims?.metadata?.role === "admin";
+
+  if (isAdminRoute(req) && !isAdmin){
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
---
+++ types/globals.d.ts
@@ -0,0 +1,14 @@
+export {};
+
+export type Roles = "admin" | "user";
+
+declare global {
+    interface CustomJwtSessionClaims {
+        metadata: {
+            role?: Roles,
+        };
+    }
+}
+
+
+
